854d6a88f13db157cc4884898531a57730c84d6b
ritesh_gatane <riteshgatane2006@gmail.com>
2026-07-15 01:46:18 +0530

    Add files via upload

---
+++ Assignment_10/program10_4.py
@@ -0,0 +1,21 @@
+def Even(No):
+    
+    for i in range(0,No+1,2):
+        print(i)
+        
+        
+    
+    
+
+
+def main():
+    Value = int(input("Enter the NUmber :"))
+
+    Even(Value)
+
+    
+
+
+
+if  __name__ =="__main__":
+    main()
