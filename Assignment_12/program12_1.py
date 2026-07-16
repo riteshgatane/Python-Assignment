aacef56bf9d060f65274f23b55a32dcc62b32a2f
ritesh_gatane <riteshgatane2006@gmail.com>
2026-07-17 00:08:04 +0530

    Add files via upload

--- Assignment_12/program12_1.py
+++ Assignment_12/program12_1.py
@@ -1,1 +1,12 @@
+#Check Vowel 
 
+char =(input("Enter the Charater to check the it is Vowel or Not: "))
+
+if(char == 'a' or char == 'e' or char == 'i' or char =='o' or char == 'u' or 
+char == 'A' or char == 'E' or char == 'I' or char =='O' or char == 'U' ):
+    print(f"{char} is a Vowel")
+
+else:
+    {
+        print(f"{char} is not a Vowel ")
+    }
